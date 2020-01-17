653fa21ab50ce9f303f8b2db34ba8000427057b7
brenckle-mark <brenckle.mark@bcg.com>
2020-01-17 17:48:58 -0500

    just cleanup and formatting

--- lib/stockfish_model.py
+++ lib/stockfish_model.py
@@ -3,27 +3,29 @@ import chess.engine
 import os
 
 
-STOCKFISH_LEVELS = {0: 1231,
-                    1: 1341,
-                    2: 1443,
-                    3: 1538,
-                    4: 1678,
-                    5: 1823,
-                    6: 1881,
-                    7: 1976,
-                    8: 2067,
-                    9: 2129,
-                    10: 2221,
-                    11: 2320,
-                    12: 2406,
-                    13: 2483,
-                    14: 2571,
-                    15: 2657,
-                    16: 2761,
-                    17: 2815,
-                    18: 2872,
-                    19: 2905,
-                    20: 3450}
+STOCKFISH_LEVELS = {
+    0: 1231,
+    1: 1341,
+    2: 1443,
+    3: 1538,
+    4: 1678,
+    5: 1823,
+    6: 1881,
+    7: 1976,
+    8: 2067,
+    9: 2129,
+    10: 2221,
+    11: 2320,
+    12: 2406,
+    13: 2483,
+    14: 2571,
+    15: 2657,
+    16: 2761,
+    17: 2815,
+    18: 2872,
+    19: 2905,
+    20: 3450,
+}
 
 
 class stockfishPlayer:
@@ -37,9 +39,11 @@ class stockfishPlayer:
         self.engine = chess.engine.SimpleEngine.popen_uci(self.path)
 
     def play(self, board):
-        result = self.engine.play(board, chess.engine.Limit(time=self.time), options={"Skill Level": self.skill})
+        result = self.engine.play(
+            board, chess.engine.Limit(time=self.time), options={"Skill Level": self.skill}
+        )
         return result.move.uci()
-    
+
     def quit(self):
         self.engine.quit()
         return True
